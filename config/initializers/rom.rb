7637184b1eb65a64efa76ec65933e3116fadbf30
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-04-04 03:09:00 +0900

    remove active_record in application.rb

--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -1,3 +1,3 @@
 ROM::Rails::Railtie.configure do |config|
-  config.gateways[:default] = [:sql, ENV.fetch('DATABASE_URL')]
+  config.gateways[:default] = [:sql, 'mysql2://root@localhost:3306/rom-practice_development']
 end
